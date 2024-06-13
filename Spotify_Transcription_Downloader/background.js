1932c0cdb83fcdd602f3193538bcc73aab2e1ece
wuhao <liuwuhaoo@gmail.com>
2024-06-13 17:20:35 +0200

    first commit

---
+++ Spotify_Transcription_Downloader/background.js
@@ -0,0 +1,142 @@
+// background.js
+let requestsMap = new Map();
+const savedIds = [];
+
+const urls = [
+    "https://open.spotify.com/*",
+    "https://episode-transcripts.spotifycdn.com/*",
+    "https://spclient.wg.spotify.com/*",
+];
+
+function getEpisodeId(url) {
+    if (!url.includes("spclient.wg.spotify.com/transcript-read-along")) {
+        return false;
+    }
+    try {
+        const pathname = new URL(url).pathname;
+        const urlSegments = pathname.split("/");
+        return urlSegments[urlSegments.length - 1];
+    } catch (error) {
+        console.error("Error parsing episode ID from URL:", error);
+        return false;
+    }
+}
+
+// Intercept and store request headers
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function (details) {
+        const episodeId = getEpisodeId(details.url);
+        if (episodeId != false || savedIds.includes(episodeId)) {
+            requestsMap.set(details.requestId, {
+                requestHeaders: details.requestHeaders,
+                episodeId,
+            });
+        }
+        return { requestHeaders: details.requestHeaders };
+    },
+    { urls },
+    ["requestHeaders"]
+);
+
+// background.js
+chrome.webRequest.onCompleted.addListener(
+    function (details) {
+        const episodeId = getEpisodeId(details.url);
+        if (
+            episodeId == false ||
+            savedIds.includes(episodeId) ||
+            !requestsMap.has(details.requestId)
+        ) {
+            return;
+        }
+        console.log(details.url);
+        if (details.responseHeaders) {
+            // console.log(details.url);
+            const contentTypeHeader = details.responseHeaders.find(
+                (header) => header.name.toLowerCase() === "content-type"
+            );
+            if (
+                contentTypeHeader &&
+                contentTypeHeader.value.includes("application/json")
+            ) {
+                const request = requestsMap.get(details.requestId);
+                const requestHeaders = request.requestHeaders;
+                const headers = new Headers();
+                requestHeaders.forEach((header) => {
+                    headers.append(header.name, header.value);
+                });
+                // Fetch the response body
+                fetch(details.url, {
+                    method: details.method,
+                    headers: headers,
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log("JSON Response:", data);
+
+                        chrome.storage.local.set(
+                            { [request.episodeId]: data },
+                            () => {
+                                console.log(
+                                    "JSON response saved to local storage for url:",
+                                    request.episodeId
+                                );
+                            }
+                        );
+                        savedIds.push(request.episodeId);
+                        chrome.runtime.sendMessage({
+                            message: "jsonSaved",
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching JSON response:", error);
+                    });
+            }
+        }
+    },
+    { urls },
+    ["responseHeaders"]
+);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "saveJson") {
+        const { episodeId } = request.data;
+        chrome.storage.local.get([episodeId], (result) => {
+            if (result[episodeId]) {
+                const jsonString = JSON.stringify(result[episodeId], null, 2);
+
+                const dataUrl =
+                    "data:application/json;charset=utf-8," +
+                    encodeURIComponent(jsonString);
+
+                chrome.downloads.download(
+                    {
+                        url: dataUrl,
+                        filename: episodeId + ".json",
+                        saveAs: true,
+                    },
+                    (downloadId) => {
+                        if (chrome.runtime.lastError) {
+                            console.error(
+                                "Error downloading file:",
+                                chrome.runtime.lastError
+                            );
+                        } else {
+                            console.log(
+                                "Download started with ID:",
+                                downloadId
+                            );
+                        }
+                    }
+                );
+            }
+        });
+    }
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    chrome.tabs.sendMessage(tabId, {
+        message: "urlChanged",
+        url: changeInfo.url,
+    });
+});
---
+++ Spotify_Transcription_Downloader/content.js
@@ -0,0 +1,55 @@
+// content.js
+function getEpisodeIdFromPageURL() {
+    const url = window.location.href;
+    if (!url.includes("open.spotify.com/episode")) {
+        return false;
+    }
+    try {
+        const pathname = new URL(url).pathname;
+        const urlSegments = pathname.split("/");
+        return urlSegments[urlSegments.length - 1];
+    } catch (error) {
+        console.error("Error parsing episode ID from URL:", error);
+        return false;
+    }
+}
+
+const button = document.createElement("button");
+button.textContent = "Download Transcript";
+button.style.position = "fixed";
+button.style.bottom = "10px";
+button.style.right = "10px";
+button.style.zIndex = 1000;
+button.style.visibility = "hidden";
+
+button.addEventListener("click", () => {
+    const episodeId = getEpisodeIdFromPageURL();
+    if (episodeId) {
+        chrome.runtime.sendMessage({
+            action: "saveJson",
+            data: {episodeId },
+        });
+    }
+});
+
+document.body.appendChild(button);
+
+function genButton() {
+    const episodeId = getEpisodeIdFromPageURL();
+    if (!episodeId) {
+        button.style.visibility = "hidden";
+        return;
+    }
+    chrome.storage.local.get([episodeId], (result) => {
+        button.style.visibility = result[episodeId]? "visible" : "hidden";
+    });
+}
+
+genButton();
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if ([ "jsonSaved", "urlChanged" ].includes(request.message)) {
+        genButton();
+    }
+});
+
